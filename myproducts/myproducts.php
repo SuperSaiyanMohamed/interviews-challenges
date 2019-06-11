f6fe43504488f5abd791c5853f37f46cfcec46f8
Mohamed Elsayed <hamusthekiller2030@gmail.com>
2019-06-11 12:56:08 +0300

    Changed the code
    Changed the code according to the past comments on it

--- myproducts/myproducts.php
+++ myproducts/myproducts.php
@@ -1,4 +1,4 @@
-<?php include 'options.php';
+<?php 
 /*
 Plugin Name: My Products
 Description: We will display a number of products you specify
@@ -15,20 +15,31 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.txt
 //    exit('Go AWAY!');
 //}
 
-// function to recall data from database to page
+// Include the options page to get number of products from it
+include 'options.php';
 
+// function to recall data from database to page
 function readdata(){
-    // connect to database
-    $connection = mysqli_connect('localhost' , 'root' , '' , 'datatest');
-    // check if the connection is not made
-    if (!$connection){
-        die("Database Connection broken");
+      //Calling the wordpress database global variable to interact with the wordpress database
+      global $wpdb;
+//    // connect to database
+//    $connection = mysqli_connect('localhost' , 'root' , '' , 'datatest');
+//    // check if the connection is not made
+//    if (!$connection){
+//        die("Database Connection broken");
+//    }
+//    // getting the value of count from options page
+//    $query_count = "SELECT * FROM wp_count ";
+//    $result_count = mysqli_query($connection, $query_count);
+//    $value = mysqli_fetch_assoc($result_count);
+//    $count = $value['value_count'];
+    
+    //if the connection of the database not made or broke for any reason
+    if(!$wpdb){
+        wp_die('Database Connection broken');
     }
-    // getting the value of count from options page
-    $query_count = "SELECT * FROM wp_count ";
-    $result_count = mysqli_query($connection, $query_count);
-    $value = mysqli_fetch_assoc($result_count);
-    $count = $value['value_count'];
+    //getting the value of count from options page
+    $count = $wpdb->get_var("SELECT value_count FROM wp_count");
     
 // This is another deprecated code to form the data with another image variable but also didn't show images    
     
@@ -55,38 +66,85 @@ function readdata(){
 //    }
     
     
-// begining to call data from the database    
-$query = "SELECT * FROM wp_posts WHERE post_status = 'publish' AND post_type LIKE 'product%'";
-// check if the query call outputs data
-if ($result = mysqli_query($connection, $query)) {
+//// begining to call data from the database    
+//$query = "SELECT * FROM wp_posts WHERE post_status = 'publish' AND post_type LIKE 'product%'";
+//// check if the query call outputs data
+//if ($result = mysqli_query($connection, $query)) {
+//    // design a table to hold the data
+//    echo "<table>";
+//    echo "<thead>";
+//    echo "<tr>";
+//    echo "<td>ID</td><td>Product Name</td><td>Place</td><td>Image</td>";
+//    echo "</tr>"; 
+//    echo "<thead>";
+//    // a counter to keep the condition we specified in options page in check
+//    $i=0;
+//    // fetch data from the database
+//    while ($row = mysqli_fetch_assoc($result)) {
+//        // calling the ID of the row to use it in further queries
+//        $post_id = $row["ID"];
+//        echo "<tr>";
+//        echo "<td style= 'width:70px; height:20px;'>".$post_id."</td>";
+//        echo "<td style= 'width:100px; height:100px;'>".$row["post_title"]."</td>";
+//        echo "<td>".$row["guid"]."</td>";
+//        // calling a specific value (meta_value) from the database when the conditions are met
+//        $query_img = "SELECT meta_value FROM wp_postmeta WHERE meta_key ='_thumbnail_id' AND post_id = $post_id";
+//        $result_img = mysqli_query($connection, $query_img);
+//        $img = mysqli_fetch_assoc($result_img);
+//        // calling another specific value that was dependant on last query
+//        $query_img_2 = "SELECT meta_value FROM wp_postmeta WHERE meta_key ='_wp_attached_file' AND post_id = ".$img['meta_value']."";
+//        $result_img_2 = mysqli_query($connection, $query_img_2);
+//        $img_2 = mysqli_fetch_assoc($result_img_2);
+//        // putting the image in the table
+//        $img_path = wp_upload_dir();
+//        echo "<td><a><img src = '".$img_path['url']."/".$img_2["meta_value"]."' style= 'width:150px; height:150px;'></a></td>";
+//        echo "</tr>";
+//        $i++;
+//        // check if the counter reached the same value as the options value called from database
+//        if ($i==$count){
+//            break;
+//        }
+//    }
+//    // closing tags
+//    echo "</table>";
+//
+//    // free result set
+//    $result->close();
+//}
+    
+    
+    
+// begining to call data from the database
+$rows = $wpdb->get_results("SELECT * FROM wp_posts WHERE post_status = 'publish' AND post_type LIKE 'product%'", ARRAY_A);
     // design a table to hold the data
     echo "<table>";
     echo "<thead>";
     echo "<tr>";
-    echo "<td>ID</td><td>Product Name</td><td>Place</td><td>Image</td>";
+    echo "<td>ID</td><td>Product Name</td><td>Category</td><td>Image</td>";
     echo "</tr>"; 
     echo "<thead>";
     // a counter to keep the condition we specified in options page in check
     $i=0;
     // fetch data from the database
-    while ($row = mysqli_fetch_assoc($result)) {
+    foreach ($rows as $row) {
         // calling the ID of the row to use it in further queries
-        $post_id = $row["ID"];
+        $post_id = $row['ID'];
         echo "<tr>";
-        echo "<td style= 'width:70px; height:20px;'>".$post_id."</td>";
-        echo "<td style= 'width:100px; height:100px;'>".$row["post_title"]."</td>";
-        echo "<td>".$row["guid"]."</td>";
+        // adding product id
+        echo "<td style= 'width:70px; height:20px;'>". $post_id ."</td>";
+        // adding product title
+        echo "<td style= 'width:100px; height:100px;'>". $row['post_title'] ."</td>";
+        // adding product type
+        echo "<td>". $row['post_type'] ."</td>";
         // calling a specific value (meta_value) from the database when the conditions are met
-        $query_img = "SELECT meta_value FROM wp_postmeta WHERE meta_key ='_thumbnail_id' AND post_id = $post_id";
-        $result_img = mysqli_query($connection, $query_img);
-        $img = mysqli_fetch_assoc($result_img);
+        $img = $wpdb->get_var("SELECT meta_value FROM wp_postmeta WHERE meta_key ='_thumbnail_id' AND post_id = $post_id");
         // calling another specific value that was dependant on last query
-        $query_img_2 = "SELECT meta_value FROM wp_postmeta WHERE meta_key ='_wp_attached_file' AND post_id = ".$img['meta_value']."";
-        $result_img_2 = mysqli_query($connection, $query_img_2);
-        $img_2 = mysqli_fetch_assoc($result_img_2);
+        $img_2 = $wpdb->get_var("SELECT meta_value FROM wp_postmeta WHERE meta_key ='_wp_attached_file' AND post_id = ".$img["meta_value"]."");
         // putting the image in the table
+        // calling the media folder in wordpress
         $img_path = wp_upload_dir();
-        echo "<td><a><img src = '".$img_path['url']."/".$img_2["meta_value"]."' style= 'width:150px; height:150px;'></a></td>";
+        // adding the img tag and putting the img in it
+        echo "<td><a><img src = '".$img_path['baseurl']."/".$img_2["meta_value"]."' style= 'width:150px; height:150px;'></a></td>";
         echo "</tr>";
         $i++;
         // check if the counter reached the same value as the options value called from database
@@ -97,11 +155,7 @@ if ($result = mysqli_query($connection, $query)) {
     // closing tags
     echo "</table>";
 
-    // free result set
-    $result->close();
 }
-    
-   // return $html;
-}
+// adding shortcode to be used in the plugin
 add_shortcode('my_random_products','readdata');
 ?>
--- myproducts/options.php
+++ myproducts/options.php
@@ -1,25 +1,52 @@
 <?php
+
+//Calling the wordpress database global variable to interact with the wordpress database
+global $wpdb;
+
+// A path for the icon of the settings page
+$plugin_setting_picture = plugin_dir_path( __FILE__ ) . 'settings.png';
+
 //Check if the value in the options text exists!
 if (isset($_POST['new_option_name'])){
     //put the value of options into variable
     $value = $_POST['new_option_name'];
-    //connect to database
-    $connection = mysqli_connect('localhost','root','','datatest');
-    //check if connection is made!
-    if($connection){
-        //select the count value in the database and store the new value in it
-        if (mysqli_query($connection,'SELECT * FROM wp_usermeta WHERE meta_key = "count"')){
-            $query = "UPDATE wp_count SET value_count = $value";
-            $result = mysqli_query($connection,$query);
-        }
-        //check if the query carries on
-        if(!$result){
-            die('Query Failed');
-        }
+//    //connect to database
+//    $connection = mysqli_connect('localhost','root','','datatest');
+//    //check if connection is made!
+//    if($connection){
+//        //select the count value in the database and store the new value in it
+//        if (mysqli_query($connection,'SELECT * FROM wp_usermeta WHERE meta_key = "count"')){
+//            $query = "UPDATE wp_count SET value_count = $value";
+//            $result = mysqli_query($connection,$query);
+//        }
+//        //check if the query carries on
+//        if(!$result){
+//            die('Query Failed');
+//        }
+//    }
+//    //condition for breaking connection with the database
+//    else{
+//        die('Database Connection Broken');
+//    }
+    
+    //check if the database connection is made
+    if($wpdb){
+    //Insert a new table with the name wp_count with the arguments id_count & value_count
+    //or update the table or it already exists
+    $wpdb->replace('wp_count',
+                    array(
+                            'id_count'=> 1,
+                            'value_count'=> $value
+                         ),
+                    array(
+                            '%d',
+                            '%d'
+                         )
+                  );
     }
-    //condition for breaking connection with the database
+    //if the database connection not made or dead exit the wordpress project with this message
     else{
-        die('Database Connection Broken');
+        wp_die('Database Connection Broken');
     }
     
     
@@ -31,7 +58,7 @@ add_action('admin_menu', 'my_cool_plugin_create_menu');
 function my_cool_plugin_create_menu() {
 
 	//create new top-level menu
-	add_menu_page('My Random Products', 'My Products', 'administrator', __FILE__, 'my_cool_plugin_settings_page' , plugins_url('/images/icon.png', __FILE__) );
+	add_menu_page('My Random Products', 'My Products', 'administrator', __FILE__, 'my_cool_plugin_settings_page' , $plugin_setting_picture );
 
 	//call register settings function
 	add_action( 'admin_init', 'register_my_cool_plugin_settings' );
@@ -59,7 +86,7 @@ function my_cool_plugin_settings_page() {
         </tr>
     </table>
     
-    <?php/* button to click on */ submit_button(); ?>
+    <?php /* button to click on */ submit_button(); ?>
 </form>
 </div>
 <?php } ?>
