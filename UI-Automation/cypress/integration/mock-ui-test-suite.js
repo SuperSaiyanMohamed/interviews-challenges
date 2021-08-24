c163dee786031f2f7a01832b274c3db1a0b31f45
Mohamed ElSayed <mohamedelsayed3580@gmail.com>
2021-08-25 02:43:31 +0300

    Add first test suite and write some test cases in it

---
+++ UI-Automation/cypress/integration/mock-ui-test-suite.js
@@ -0,0 +1,25 @@
+describe("Mock UI Automation", () => {
+    beforeEach(() => {
+        cy.visit("/");
+        cy.contains("a","Sign in").click();
+    });
+    xit("Testing with empty input in email address field", () =>{
+        cy.get("#email_create").type("text");
+        cy.get("#SubmitCreate").click();
+        cy.contains("li","Invalid email address.");
+    });
+    xit("Testing with valid email address", () =>{
+        cy.get("#email_create").type("mohamed@gmail.com");
+        cy.get("#SubmitCreate").click();
+        cy.url().should('include', '#account-creation');
+    });
+    xit("Leave register fields empty and click register", () =>{
+        cy.get("#email_create").type("mohamed@gmail.com");
+        cy.get("#SubmitCreate").click();
+        cy.get("#submitAccount").click();
+        cy.contains("p", "There are 8 errors");
+    });
+    it("Add valid firstName only", () =>{
+        
+    });
+});
