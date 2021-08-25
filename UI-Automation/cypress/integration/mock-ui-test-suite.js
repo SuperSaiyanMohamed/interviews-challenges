ebe74bd41ded5f47b79e21e671a31c730be0eba1
Mohamed ElSayed <mohamedelsayed3580@gmail.com>
2021-08-25 03:03:31 +0300

    Add valid password only test case

--- UI-Automation/cypress/integration/mock-ui-test-suite.js
+++ UI-Automation/cypress/integration/mock-ui-test-suite.js
@@ -26,11 +26,19 @@ describe("Mock UI Automation", () => {
         cy.get("#submitAccount").click();
         cy.get("ol").should("not.have.text", "firstname");
     });
-    it("Add valid lastName only", () =>{
+    xit("Add valid lastName only", () =>{
         cy.get("#email_create").type("mohamed@gmail.com");
         cy.get("#SubmitCreate").click();
         cy.get("#customer_lastname").type("Mohamed");
         cy.get("#submitAccount").click();
         cy.get("ol").should("not.have.text", "lastname");
+    });
+    it("Add valid password only", () =>{
+        cy.get("#email_create").type("mohamed@gmail.com");
+        cy.get("#SubmitCreate").click();
+        cy.wait(2000);
+        cy.get("#passwd").type("abcde");
+        cy.get("#submitAccount").click();
+        cy.get("ol").should("not.have.text", "passwd");
     });
 });
